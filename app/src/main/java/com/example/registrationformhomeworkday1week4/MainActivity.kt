30f7f10b8cc7facab2402a627c7a0f293a86e38b
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-01 09:44:12 +0300

    last update

--- app/src/main/java/com/example/registrationformhomeworkday1week4/MainActivity.kt
+++ app/src/main/java/com/example/registrationformhomeworkday1week4/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.registrationformhomeworkday1week4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.registrationformhomeworkday1week4.databinding.ActivityMainBinding
-import com.google.android.material.resources.TextAppearance
-import java.text.Format
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -15,14 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     binding.registrationButtonId.setOnClickListener { showInfo() }
     }
-    val name = binding.Name.text
-    val email = binding.email.text
-    val pass1 = binding.password.text
-    val pass2 = binding.repassword.text
-    val genderF = binding.femaleBottun.isChecked
-    var result = binding.Result.text
-    val birthDate = binding.Birthdate.text
+
     fun showInfo(){
+        val name = binding.Name.text
+        val email = binding.email.text
+        val pass1 = binding.password.text
+        val pass2 = binding.repassword.text
+        val genderF = binding.femaleBottun.isChecked
+        var result = binding.Result.text
+        val birthDate = binding.Birthdate.text
         // checking if any information is empty
      val info =   if (name.toString().isEmpty() || email.toString().isEmpty() ||
             pass1.toString().isEmpty() || pass2.toString().isEmpty()) {
